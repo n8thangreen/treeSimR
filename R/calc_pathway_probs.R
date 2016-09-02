44825b890b56c472c1280ec052bc6bb55aa8011c
Nathan Green <nathan.green@imperial.ac.uk>
2016-09-02 16:10:58 +0100

    asserts and checks in calc.pathway_probs()

--- R/calc_pathway_probs.R
+++ R/calc_pathway_probs.R
@@ -1,23 +1,35 @@
 
 #' Calculate Total Pathway Probabilities of Decision Tree
 #'
-#' @param osNode
+#'
+#' @param osNode object of class costeffectiveness.tree
+#' @param FUN sum or product
 #'
 #' @return vector of values
 #' @export
 #'
 #' @examples
 #'
-calc.pathway_probs <- function(osNode){
+calc.pathway_probs <- function(osNode, FUN = "product"){
+
+  stopifnot("costeffectiveness.tree" %in% class(osNode))
+  FUN <- match.arg(FUN, c("sum", "product"))
 
   x <- rep(osNode$Get("p")[1], osNode$totalCount)
-  x[is.na(x)] <- 0  ##TODO## depending on sum or prod
+  x[is.na(x)] <- 1  ##TODO## depending on sum or prod
 
   for(i in 2:osNode$totalCount){
 
     currentCount <- Get(t, "totalCount")[i]
     pos <- i + currentCount - 1
-    x[i:pos] <- x[i:pos] + rep(osNode$Get("p")[i], currentCount)
+
+    if (FUN=="product"){
+      x[i:pos] <- x[i:pos] * rep(osNode$Get("p")[i], currentCount)
+    }else if (FUN=="sum"){
+      x[i:pos] <- x[i:pos] + rep(osNode$Get("p")[i], currentCount)
+    }else{
+      stop("Error: unknown operator")
+    }
   }
 
   names(x) <- NULL
