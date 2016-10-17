004b240028c1fdd201811a3c0fcc2bf3dd48f0a9
Nathan Green <n8thangreen@yahoo.co.uk>
2016-10-17 16:28:10 +0100

    debug get_start_state_proportions

--- R/get_start_state_proportions.R
+++ R/get_start_state_proportions.R
@@ -1,24 +1,28 @@
 #' Get Start State Proportions
 #'
-#' @param path_probs
-#' @param startstateid
-#' @param samplesize
-#' @param numsamples
+#' Population sizes of individuals in each starting state as input for the competing risks model.
 #'
-#' @return
+#' @param path_probs  Probabilitites along each branch, from root to leaf usinghj \link{calc_pathway_probs}
+#' @param startstateid How to group terminal nodes
+#' @param samplesize Total population size
+#' @param numsamples Number of realisations
+#'
+#' @return list
 #' @export
 #'
 #' @examples
+#'
 get_start_state_proportions <- function(path_probs, startstateid, samplesize, numsamples){
 
   stopifnot(length(path_probs)==length(startstateid))
 
-  nterminal <- length(path_probs)
-  sample.mat <- matrix(NA, nrow = nterminal, ncol = numsamples)
+  nterminal  <- length(path_probs)
+  sample.mat <- matrix(0, nrow = nterminal, ncol = numsamples)
   for (i in 1:numsamples){
 
-    sample.mat[,i] <- table(sample(x = 1:nterminal, size = samplesize, prob = path_probs, replace = TRUE))/samplesize
+    tab <- table(sample(x = 1:nterminal, size = samplesize, prob = path_probs, replace = TRUE))/samplesize
+    sample.mat[as.numeric(names(tab)), i] <- tab
   }
 
-  apply(sample.mat, 2, function(x) aggregate(x, by=list(startstateid), FUN=sum))
+  apply(sample.mat, 2, function(x) setNames(object = aggregate(x, by=list(startstateid), FUN=sum), nm = c("state", "prob")))
 }
